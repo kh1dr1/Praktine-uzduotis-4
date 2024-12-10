b8ec7c91b101726010d609b607e418b32ae711c8
kh1dr1 <1rd1hk@proton.me>
2024-12-10 16:20:00 +0200

    Saskaita

--- main.cpp
+++ main.cpp
@@ -1,46 +1,40 @@
+#include <ios>
 #include <iostream>
+#include <iterator>
 #include <string>
 
 using namespace std;
 
-const int MAX_ORDER_LIST_SIZE = 20;
+const int MAX_ORDER_COUNT = 20;
 const string RES_NAME = "Du skoniai";
+const string EURO_SIGN = "€";
+const string DELIM = " | ";
 
-struct MenuItemType {
+struct MenuItem_T {
 	string menuItem;
-	float menuPrice{};
+	float itemPrice = 0;
+	int amount = 1; // porciju skaicius
 };
 
-const MenuItemType MENU_LIST[] = {
-    {"Kiaušiniene", 1.45},
-    {"Kiaulienos šoninė su keptu kiaušiniu", 2.45},
-    {"Keksiukas su vyšnia", 0.99},
-    {"Prancūziškas skrebutis", 1.99},
-    {"Vaisių salotos", 2.49},
-    {"Pusryčių dribsniai", 0.69},
-    {"Kava", 0.50},
-    {"Arbata", 0.75}};
+const MenuItem_T MENU_LIST[] = {{"Kiaušiniene", 1.45},
+                                {"Kiaulienos šoninė su keptu kiaušiniu", 2.45},
+                                {"Keksiukas su vyšnia", 0.99},
+                                {"Prancūziškas skrebutis", 1.99},
+                                {"Vaisių salotos", 2.49},
+                                {"Pusryčių dribsniai", 0.69},
+                                {"Kava", 0.50},
+                                {"Arbata", 0.75}};
 
 const int MENU_SIZE = size(MENU_LIST);
 
-struct OrderItemType {
-	string menuItem;
-	float menuPrice{};
-	float amount{};
-};
-
 void showMenu() {
 	cout << "\n----- Mūsų meniu -----\n";
-
 	for (int i = 0; i < MENU_SIZE; i++) {
-		// cout << setw(2) << i + 1 << setw(40) << MENU_LIST[i].menuItem <<
-		// setw(5) << MENU_LIST[i].menuPrice << '\n';
-		cout << i + 1 << ". " << MENU_LIST[i].menuItem << " - "
-		     << MENU_LIST[i].menuPrice << " €\n";
+		cout << i + 1 << ". " << MENU_LIST[i].menuItem << DELIM << MENU_LIST[i].itemPrice << ' ' << EURO_SIGN << '\n';
 	}
 }
 
-int choicePrompt() {
+int getChoice() {
 	int choice = 0;
 
 	cout << "\n----- " << RES_NAME << " -----\n";
@@ -55,27 +49,80 @@ int choicePrompt() {
 	return choice;
 }
 
+// patiekalu uzsakymas
+// returns --> success
+bool getData(MenuItem_T orderList[], int &lastOrderIndex) {
+	int order = 0;
+	int count = 1;
+	static int num = 1;
+
+	// cout << "\nPasirinkite patiekalą ir įveskite porcijų skaičių.\n";
+	cout << "Jūsų pasirinkimas nr. " << num << " (patiekalas, porcijos): ";
+	cin >> order >> count;
+
+	if (order >= 1 && order <= MENU_SIZE) {
+		order--;
+
+		// strukturos inicializavimas
+		orderList[lastOrderIndex] = MenuItem_T{MENU_LIST[order].menuItem, MENU_LIST[order].itemPrice, count};
+
+		// orderList[lastOrderIndex].menuItem = MENU_LIST[order].menuItem;
+		// orderList[lastOrderIndex].itemPrice = MENU_LIST[order].itemPrice;
+		// orderList[lastOrderIndex].amount = count;
+
+		lastOrderIndex++;
+		num++;
+
+		return true;
+	}
+
+	return false;
+}
+
 int main() {
 	cout << "\nSveiki atvykę i restoraną \"" << RES_NAME << "\"\n";
 
-	OrderItemType order_list[MAX_ORDER_LIST_SIZE];
-	int choice = -1;
+	MenuItem_T order_list[MAX_ORDER_COUNT];
+	int lastOrderListItem = 0;
 
-	while (choice != 0) {
-		choice = choicePrompt();
-		switch (choice) {
+	// uzsakymu saraso inicializacija
+	// for (int i = 0; i < MAX_ORDER_COUNT; i++) {
+	// 	order_list[i] = MenuItem_T{};
+	// }
+
+	int actionChoice = -1;
+	bool orderSuccess = true;
+
+	while (actionChoice != 0) {
+		orderSuccess = true; // reset
+		actionChoice = getChoice();
+		switch (actionChoice) {
 			case 1:
 				showMenu();
 				break;
 			case 2:
+				showMenu();
+				while (orderSuccess) {
+					orderSuccess = getData(order_list, lastOrderListItem);
+				}
 				break;
 			case 3:
+				cout << "empty? " << boolalpha << empty(order_list) << '\n';
+				cout << "\nJūsų užsakymas:\n";
+				for (int i = 0; i < MAX_ORDER_COUNT; i++) {
+					MenuItem_T item = order_list[i];
+					if (item.menuItem.empty()) {
+						break;
+					}
+					cout << item.menuItem << DELIM << item.itemPrice << ' ' << EURO_SIGN << DELIM << 'x' << item.amount
+					     << '\n';
+				}
 				break;
 			case 0:
 				cout << "\nLauksime sugrįžtant..!\n\n";
 				break;
 			default:
-				cout << "[info] Neteisingas pasirinkimas!\n";
+				cout << "Neteisingas pasirinkimas.\n";
 				break;
 		}
 	}
