fd675313aab334458cd0fd461cb7ab599bb9f435
kh1dr1 <1rd1hk@proton.me>
2024-12-11 18:23:05 +0200

    printCheck funkcija

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <ios>
 #include <iostream>
 #include <iterator>
@@ -9,28 +10,29 @@ const int MAX_ORDER_COUNT = 20;
 const string RES_NAME = "Du skoniai";
 const string EURO_SIGN = "€";
 const string DELIM = " | ";
+const double PVM = 0.21;
 
 struct MenuItem_T {
 	string menuItem;
-	float itemPrice = 0;
+	double itemPrice = 0;
 	int amount = 1; // porciju skaicius
 };
 
-const MenuItem_T MENU_LIST[] = {{"Kiaušiniene", 1.45},
-                                {"Kiaulienos šoninė su keptu kiaušiniu", 2.45},
-                                {"Keksiukas su vyšnia", 0.99},
-                                {"Prancūziškas skrebutis", 1.99},
-                                {"Vaisių salotos", 2.49},
-                                {"Pusryčių dribsniai", 0.69},
-                                {"Kava", 0.50},
-                                {"Arbata", 0.75}};
+const MenuItem_T MENU[] = {{"Kiaušiniene", 1.45},
+                           {"Kiaulienos šoninė su keptu kiaušiniu", 2.45},
+                           {"Keksiukas su vyšnia", 0.99},
+                           {"Prancūziškas skrebutis", 1.99},
+                           {"Vaisių salotos", 2.49},
+                           {"Pusryčių dribsniai", 0.69},
+                           {"Kava", 0.50},
+                           {"Arbata", 0.75}};
 
-const int MENU_SIZE = size(MENU_LIST);
+const int MENU_SIZE = size(MENU);
 
 void showMenu() {
 	cout << "\n----- Mūsų meniu -----\n";
 	for (int i = 0; i < MENU_SIZE; i++) {
-		cout << i + 1 << ". " << MENU_LIST[i].menuItem << DELIM << MENU_LIST[i].itemPrice << ' ' << EURO_SIGN << '\n';
+		cout << i + 1 << ". " << MENU[i].menuItem << DELIM << MENU[i].itemPrice << ' ' << EURO_SIGN << '\n';
 	}
 }
 
@@ -50,45 +52,57 @@ int getChoice() {
 }
 
 // patiekalu uzsakymas
-// returns --> success
-bool getData(MenuItem_T orderList[], int &lastOrderIndex) {
+bool getData(MenuItem_T menuList[], int &lastOrderIndex) {
 	int order = 0;
 	int count = 1;
-	static int num = 1;
 
-	// cout << "\nPasirinkite patiekalą ir įveskite porcijų skaičių.\n";
-	cout << "Jūsų pasirinkimas nr. " << num << " (patiekalas, porcijos): ";
+	cout << "Jūsų pasirinkimas (patiekalas, porcijos): ";
 	cin >> order >> count;
 
 	if (order >= 1 && order <= MENU_SIZE) {
 		order--;
 
 		// strukturos inicializavimas
-		orderList[lastOrderIndex] = MenuItem_T{MENU_LIST[order].menuItem, MENU_LIST[order].itemPrice, count};
-
-		// orderList[lastOrderIndex].menuItem = MENU_LIST[order].menuItem;
-		// orderList[lastOrderIndex].itemPrice = MENU_LIST[order].itemPrice;
-		// orderList[lastOrderIndex].amount = count;
+		menuList[lastOrderIndex] = MenuItem_T{MENU[order].menuItem, MENU[order].itemPrice, count};
 
 		lastOrderIndex++;
-		num++;
-
 		return true;
 	}
 
 	return false;
 }
 
+void printCheck(MenuItem_T menuList[]) {
+	double sum = 0;
+	double pvm = 0;
+	double finalSum = 0;
+
+	cout << "\n---- Jūsų užsakymas ----\n";
+	for (int i = 0; i < MAX_ORDER_COUNT; i++) {
+		MenuItem_T item = menuList[i];
+		if (item.menuItem.empty()) {
+			break;
+		}
+		sum += (item.itemPrice * item.amount);
+		cout << item.menuItem << DELIM << item.itemPrice << ' ' << EURO_SIGN << DELIM << item.amount << " porcija(-os)"
+		     << '\n';
+	}
+
+	pvm = sum * PVM;
+	finalSum = sum + pvm;
+
+	cout << "\n---- Sąskaita ----\n";
+	cout << fixed << setprecision(2);
+	cout << "Kaina be PVM: " << sum << ' ' << EURO_SIGN << '\n';
+	cout << "PVM suma: " << pvm << ' ' << EURO_SIGN << '\n';
+	cout << "Galutinė kaina (su PVM): " << finalSum << ' ' << EURO_SIGN << '\n';
+}
+
 int main() {
 	cout << "\nSveiki atvykę i restoraną \"" << RES_NAME << "\"\n";
 
-	MenuItem_T order_list[MAX_ORDER_COUNT];
+	MenuItem_T menuList[MAX_ORDER_COUNT];
 	int lastOrderListItem = 0;
-
-	// uzsakymu saraso inicializacija
-	// for (int i = 0; i < MAX_ORDER_COUNT; i++) {
-	// 	order_list[i] = MenuItem_T{};
-	// }
 
 	int actionChoice = -1;
 	bool orderSuccess = true;
@@ -101,25 +115,16 @@ int main() {
 				showMenu();
 				break;
 			case 2:
-				showMenu();
+				cout << '\n';
 				while (orderSuccess) {
-					orderSuccess = getData(order_list, lastOrderListItem);
+					orderSuccess = getData(menuList, lastOrderListItem);
 				}
 				break;
 			case 3:
-				cout << "empty? " << boolalpha << empty(order_list) << '\n';
-				cout << "\nJūsų užsakymas:\n";
-				for (int i = 0; i < MAX_ORDER_COUNT; i++) {
-					MenuItem_T item = order_list[i];
-					if (item.menuItem.empty()) {
-						break;
-					}
-					cout << item.menuItem << DELIM << item.itemPrice << ' ' << EURO_SIGN << DELIM << 'x' << item.amount
-					     << '\n';
-				}
+				printCheck(menuList);
 				break;
 			case 0:
-				cout << "\nLauksime sugrįžtant..!\n\n";
+				cout << "\nLauksime sugrįžtant!\n\n";
 				break;
 			default:
 				cout << "Neteisingas pasirinkimas.\n";
